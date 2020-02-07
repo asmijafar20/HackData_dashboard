ff9a93ec8492fbf8a6c55262fb74797e1e5cf509
Asmi Jafar <47150162+asmijafar20@users.noreply.github.com>
2020-02-07 13:52:54 +0530

    Add files via upload

---
+++ data.py
@@ -0,0 +1,27 @@
+def Articles():
+    articles = [
+
+          {
+             'id': 1,
+             'title': 'Article One',
+             'body': 'Loremikjbbjd',
+             'author': 'hannan',
+             'create date': '29-1-2020'
+          },
+          {
+             'id': 2,
+             'title': 'Article Two',
+             'body': 'Loremikjbbjd',
+             'author': 'surbhi sharma',
+             'create date': '30-1-2020'
+          },
+           {
+             'id': 3,
+             'title': 'Article Three',
+             'body': 'Loremikjbbjd',
+             'author': 'kulsoom',
+             'create date': '31-1-2020'
+          }
+
+    ]
+    return articles
